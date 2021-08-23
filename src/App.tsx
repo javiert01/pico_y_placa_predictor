914a58c26dde98dfedb221bcbbcd7c71bf257133
javiertinoco01 <javier.tinoco@trade.ec>
2021-08-22 23:02:36 -0500

    Add description to app and included PicoyPlacaForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import PicoyPlacaForm from "./components/PicoyPlacaForm/PicoyPlacaForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="main">
+      <h1>Pico y Placa Predictor</h1>
+      <p>This webpage let's you know if your car can be on the road based on Quito's Pico y Placa rules.</p>
+      <p>Enter your license plate, a day and a time and click on the "View Result!" button.</p>
+      <PicoyPlacaForm />
+    </div>
+  );
 }
 
 export default App;
