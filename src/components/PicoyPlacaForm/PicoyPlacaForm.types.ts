24eede5fac9c18768afd2e8539bdcfec6016cf5d
javiertinoco01 <javier.tinoco@trade.ec>
2021-08-22 19:54:27 -0500

    added Types for the PicoyPlaca Form component

---
+++ src/components/PicoyPlacaForm/PicoyPlacaForm.types.ts
@@ -0,0 +1,8 @@
+// Interfaces and types from component PicoyPlacaForm
+
+// PicoyPlacaFormData type
+export interface PicoyPlacaFormData {
+    licensePlate: string;
+    day: string;
+    hour: string;
+  }
