e302dfe07ed5b205be8a0a0ed68d7631ab30c3f7
javiertinoco01 <javier.tinoco@trade.ec>
2021-08-22 21:49:14 -0500

    added functions isTimeBetweenPicoyPlacaHours and isLastDigitInPicoyPlacaDay

--- src/components/PicoyPlacaForm/PicoyPlacaForm.helper.tsx
+++ src/components/PicoyPlacaForm/PicoyPlacaForm.helper.tsx
@@ -1,4 +1,6 @@
+import dayjs from "dayjs";
 import * as Yup from "yup";
+import isBetween from "dayjs/plugin/isBetween";
 export const daysOfWeek = [
   "Monday",
   "Tuesday",
@@ -17,4 +19,36 @@ export const validationSchema = () => {
     day: Yup.string().required("Required"),
     hour: Yup.string().required("Required"),
   });
+};
+
+export const isTimeBetweenPicoyPlacaHours = (time: dayjs.Dayjs) => {
+  dayjs.extend(isBetween);
+  const startMorningHour = dayjs("07:00", "HH:mm");
+  const endMorningHour = dayjs("09:30", "HH:mm");
+  const startEveningHour = dayjs("16:00", "HH:mm");
+  const endEveningHour = dayjs("19:30", "HH:mm");
+  if (time.isBetween(startMorningHour, endMorningHour)) {
+    return true;
+  }
+  if (time.isBetween(startEveningHour, endEveningHour)) {
+    return true;
+  }
+  return false;
+};
+
+export const isLastDigitInPicoyPlacaDay = (lastDigit: number, day: string) => {
+  if (lastDigit !== 0) {
+    let minusNumber = lastDigit % 2 !== 0 ? 0 : 1;
+    for (let i = 1; i <= lastDigit; i++) {
+      if (lastDigit % 2 === 0 && i % 2 === 0) {
+        minusNumber++;
+      }
+      if (lastDigit % 2 !== 0 && i % 2 !== 0) {
+        minusNumber++;
+      }
+    }
+    lastDigit -= minusNumber;
+    return day === daysOfWeek[lastDigit];
+  }
+  return day === daysOfWeek[4];
 };
