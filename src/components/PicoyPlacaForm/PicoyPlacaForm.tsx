f5fbc11a28cd12b54b828faa30edfbebd0de4257
javiertinoco01 <javier.tinoco@trade.ec>
2021-08-22 19:55:33 -0500

    add formik library components and configuration

--- src/components/PicoyPlacaForm/PicoyPlacaForm.tsx
+++ src/components/PicoyPlacaForm/PicoyPlacaForm.tsx
@@ -1,29 +1,50 @@
-import { daysOfWeek } from "./PicoyPlacaForm.helper";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { daysOfWeek, validationSchema } from "./PicoyPlacaForm.helper";
 import classes from "./PicoyPlacaForm.module.css";
 
+
 const PicoyPlacaForm = () => {
+ 
   return (
-    <form className={classes.form}>
-      <div className={classes.inputContainer}>
-        <label htmlFor="licensePlate">License Plate:</label>
-        <input type="text" required id="licensePlate" placeholder="Enter your license plate"/>
-      </div>
-      <div>
-        <label htmlFor="days">Select a day:</label>
-        <select name="days" id="days">
-          {daysOfWeek.map((day) => (
-            <option value={day}>{day}</option>
-          ))}
-        </select>
-      </div>
-      <div className={classes.inputContainer}>
-        <label htmlFor="hour">Choose an hour:</label>
-        <input type="time" required id="hour" />
-      </div>
-      <div className={classes.btnContainer}>
-        <button>View Result!</button>
-      </div>
-    </form>
+    <Formik
+      initialValues={{
+        licensePlate: "",
+        day: "",
+        hour: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log("logging values!", values);
+      }}
+    >
+      <Form className={classes.form}>
+        <div className={classes.inputContainer}>
+          <label htmlFor="licensePlate">License Plate:</label>
+          <Field type="text" name="licensePlate" />
+          <ErrorMessage name="licensePlate" />
+        </div>
+        <div className={classes.inputContainer}>
+          <label htmlFor="day">Select a day:</label>
+          <Field name="day" id="day" as="select">
+            <option value="">Select a day</option>
+            {daysOfWeek.map((day) => (
+              <option value={day} key={day}>
+                {day}
+              </option>
+            ))}
+          </Field>
+          <ErrorMessage name="day" />
+        </div>
+        <div className={classes.inputContainer}>
+          <label htmlFor="hour">Choose an hour:</label>
+          <Field name="hour" type="time" />
+          <ErrorMessage name="hour" />
+        </div>
+        <div className={classes.btnContainer}>
+          <button type="submit">View Result!</button>
+        </div>
+      </Form>
+    </Formik>
   );
 };
 
